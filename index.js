4b9847f7be18c3f019beecc78ea3be5bfae0149e
habibullah <habibullahozy298796@gmail.com>
2024-05-25 23:53:00 +0600

    added vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.use(cors())
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://mdhabibullah7520:${process.env.BD_USER_PASSWORD}@kqhtrades.fpfb50u.mongodb.net/?retryWrites=true&w=majority&appName=kqhtrades`;
-console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,6 +36,8 @@ async function run() {
 
     const itemsCollection = client.db("kqhtrades_storage").collection("products");
 
+    const categoryCollection = client.db("kqhtrades_storage").collection("category");
+
     const usersCollection = client.db("kqhtrades_storage").collection("users")
     // const products={name:"habib",post:"sljoert"}
     // const result =await itemsCollections.insertOne(products)
@@ -47,17 +49,20 @@ async function run() {
     });
 
 
-    app.put('/addusers/admin/:id', async (req, res) => {
-      const decodedEmail = req.decoded.email;
-      const query = { email: decodedEmail };
-      const user = await usersCollection.findOne(query)
-      if (user?.role !== 'admin') {
-          return res.status(403).send({ massage: 'forbiden access' })
-      }
+    app.put('/users/admin/:id', async (req, res) => {
+      // const decodedEmail = req.decoded.email;
+      // console.log(decodedEmail)
+      // const query = { email: decodedEmail };
+      // const user = await usersCollection.findOne(query)
+      // if (user?.role !== 'admin') {
+      //     return res.status(403).send({ massage: 'forbiden access' })
+      // }
 
 
       const id = req.params.id;
-      const filter = { _id: ObjectId(id) }
+      
+      const filter = { _id: new ObjectId(id) }
+      console.log(filter)
       const options = { upsert: true };
       const updatedDoc = {
           $set: {
@@ -67,6 +72,24 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
   })
+
+
+
+  app.get('/users/admin/:email', async(req, res)=>{
+    const email = req.params.email;
+    const query = { email }
+    const user = await usersCollection.findOne(query);
+    res.send({ isAdmin: user?.role === 'admin' })
+})
+
+
+app.delete('/users/delete/:id', async (req, res) => {
+  const id = req.params.id;
+  // console.log(ObjectId(id))
+  const query = { _id: new ObjectId(id) };
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+});
 
 
     app.post('/addusers', async (req, res) => {
@@ -93,6 +116,29 @@ async function run() {
 
 
 
+  app.put('/users/verify/:id', async (req, res) => {
+    const id = req.params.id
+    const filter = { _id: new ObjectId(id) }
+    const options = { upsert: true };
+    const updatedDoc = {
+        $set: {
+            quality: 'varifyed'
+        }
+    }
+    const result = await usersCollection.updateOne(filter, updatedDoc, options);
+    res.send(result)
+});
+
+
+
+app.delete('/users/delete/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+});
+
+
 
     app.get('/catagory/:id', async (req, res) => {
 
@@ -100,11 +146,28 @@ async function run() {
 
       const query = { catagory_id: (id) }
 
-      const laptops = itemsCollections.find(query);
+      const laptops = itemsCollection.find(query);
       const infoe = await laptops.toArray();
       // console.log(infoe)
       res.send(infoe)
     })
+
+
+
+    app.get('/productsDetails/:id', async (req, res) => {
+
+      const id = req.params.id;
+      console.log(id)
+
+      const query = { _id: new ObjectId(id) }
+
+      console.log(query)
+
+      const productOne = await itemsCollection.findOne(query);
+      // const infoeCatagory =await laptopsOne.toArray();
+      // console.log(infoe)
+      res.send(productOne)
+  })
 
 
     app.post('/product', async (req, res) => {
@@ -116,20 +179,49 @@ async function run() {
     })
 
 
+    // added Category
+    app.post('/category', async (req, res) => {
+      const addCategory = req.body;
+     
+      const result = await categoryCollection.insertOne(addCategory);
+
+      res.send(result)
+    })
+
+
+    app.get('/catagory', async (req, res) => {
+      const query = {}
+      const result = categoryCollection.find(query);
+      const data = await result.toArray()
+      res.send(data)
+  });
+
+
     // My products seller email based load this
-    // app.get('/products', async (req, res) => {
-    //   const email = req.query.email;
-    //   const decodedEmail = req.decoded.email;
-    //   if (email !== decodedEmail) {
-    //     return res.status(403).send({ massage: 'forbidden access' })
-    //   }
-    //   const query = { email: email }
-    //   const result = await itemsCollections.find(query).toArray();
-    //   res.send(result)
-    // })
+    app.get('/myproducts/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email)
+      // const decodedEmail = req.decoded.email;
+      // if (email !== decodedEmail) {
+      //   return res.status(403).send({ massage: 'forbidden access' })
+      // }
+      const query = { email: email }
+      console.log(query)
+      const result = await itemsCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
 
+    // seller Hook
+
+    app.get('/seller/:email', async(req, res)=>{
+      const email = req.params.email;
+      console.log(email)
+      const query = { email }
+      const seller = await usersCollection.findOne(query);
+      res.send({isSeller: seller.role === 'Seller'})
+  })
 
     //   app.get('/allProducts', async (req, res) => {
     //     const query = {}
